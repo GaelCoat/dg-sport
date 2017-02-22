848be0ebb9188c79a88dda5e3e262e6cb2527d04
GaelCoat <zoul.gaelcoat@gmail.com>
2017-02-22 12:36:14 +0100

    eng trad

--- src/lang.js
+++ src/lang.js
@@ -24,22 +24,22 @@ module.exports = {
 
   en: {
     home: {
-      subtitle: '<span>Accompagne les sportifs de haut</span><span>niveau dans le développement de leur</span><span>communauté et d’une identité digitale</span>'
+      subtitle: '<span>We guide high-level athletes in</span><span>the development of their online</span><span>presence and their community</span>'
     },
     thinking: {
-      txt: 'Nous sommes convaincus que le digital joue désormais un rôle majeur dans le monde du sport. Il permet aux sportifs de fédérer une communauté, de bâtir une image forte, de marketer leurs performances… Autant d’éléments sous évalués mais pourtant essentiels pour décupler la valeur d’un sportif à travers ses contrats en clubs, ses contrats de sponsoring & pour lui permettre de réussir ses projets annexes.',
-      quote: '<span>Le nom d\'un grand</span><span>sportif est une</span><span>marque qu\'il</span><span>convient à ce</span><span>titre de protéger et</span><span>de développer sur</span><span>le plan digital.</span>',
-      quoteRaw: '\"Le nom d\'un grand sportif est une marque qu\'il convient à ce titre de protéger et de développer sur le plan digital.\"',
+      txt: 'We believe that digital plays a key role in the world of sports. It allows athletes to develop a network, to build a strong image and market their achievements. These factors, often underestimated, are undoubtedly essential in order to emphasize the value of an athlete, help them getting better contracts or sponsorhips, and to enable them to find success in further projects.',
+      quote: '<span>The name of</span><span>a great athlete</span><span>is a brand and</span><span>as such it must</span><span>be protected</span><span>and developped</span><span>digitally.</span>',
+      quoteRaw: '\"The name of a great athlete is a brand and as such it must be protected and developped digitally.\"',
     },
     us: {
-      quote: '<span>Nous nous battons chaque jour pour bâtir,</span><span>à partir de sportifs, des héros à part entière.</span>',
-      quoteRaw: '<span>Nous nous battons chaque jour pour ériger les sportifs de haut niveau au rang d’idole. Nous bâtissons les héros de demain.</span>',
-      txt: 'Nous sommes une équipe de passionnés, d’un dévouement sans limite et d’une fiabilité sans faille. Nous fournissons un travail d’une qualité incomparable, focus sur les détails et l’innovation. Nous sommes loués par nos clients pour notre aptitude à sortir du rôle d’agence traditionnelle afin de nous positionner comme un véritable partenaire de leur carrière.'
+      quote: '<span>We strive everyday to make great athletes become</span><span>idols. We create the heroes of tomorrow.</span>',
+      quoteRaw: '<span>We strive everyday to make great athletes become idols. We create the heroes of tomorrow.</span>',
+      txt: 'We are a passionate and reliable team whose dedication is limitless. We constantly question the status quo because we are innovative and detail oriented. We have received praise from our clients thanks to our aptitude to exceed the traditional role of an agency, that allows us to be a real partner throughout their career.'
     },
     skills: {
-      ui: 'Création, refonte & optimisation<br/>de sites internet responsive',
-      identity: 'Élaboration de logos, de designs<br/>& de chartes graphiques',
-      content: 'Conception de GIF, de filtres<br/>& de montages photos',
+      ui: 'Building responsive web apps<br/>and interactive websites',
+      identity: 'Creation of logos, designs<br/>and graphic identity',
+      content: 'Creation of GIFs, filters<br/>and photo collages',
     }
   },
 
